26e51ba1f7793e5330b70862147d092206deafd6
Andrii Petrov <bucklya@gmail.com>
2020-06-12 19:40:36 +0300

    done

--- app/src/main/java/com/example/androidkotlintaskd/fragments/InfoFragment.kt
+++ app/src/main/java/com/example/androidkotlintaskd/fragments/InfoFragment.kt
@@ -9,17 +9,14 @@ import android.widget.Toast
 import com.example.androidkotlintaskd.App
 
 import com.example.androidkotlintaskd.R
-import com.example.androidkotlintaskd.db.model.DrinkDB
 import com.example.androidkotlintaskd.db.model.InfoDrinkDB
 import com.example.androidkotlintaskd.network.ApiService
-import com.example.androidkotlintaskd.network.model.Drink
 import com.example.androidkotlintaskd.network.model.InfoDrink
 import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_info.*
-import kotlin.coroutines.coroutineContext
 
 class InfoFragment(private val id: String) : Fragment() {
 
@@ -51,36 +48,39 @@ class InfoFragment(private val id: String) : Fragment() {
                     .with(activity)
                     .load(it.strDrinkThumb)
                     .into(info_photo)
-            }, { it -> showError(it) })
+            },
+                { it -> showErrorSecond(it) })
 
 
         disposable = ApiService.getDrinkInfo(id)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ it ->
-                listInfoFromServer.addAll(it.infoDrinks)
-                listInfoFromServer.forEach {
-                    dao.insertInfoDrink(
-                        InfoDrinkDB(
-                            0,
-                            it.idDrink,
-                            it.strDrink,
-                            it.strCategory,
-                            it.strAlcoholic,
-                            it.strGlass,
-                            it.strInstructions,
-                            it.strDrinkThumb
+            .subscribe(
+                { it ->
+                    listInfoFromServer.addAll(it.infoDrinks)
+                    listInfoFromServer.forEach {
+                        dao.insertInfoDrink(
+                            InfoDrinkDB(
+                                0,
+                                it.idDrink,
+                                it.strDrink,
+                                it.strCategory,
+                                it.strAlcoholic,
+                                it.strGlass,
+                                it.strInstructions,
+                                it.strDrinkThumb
+                            )
                         )
-                    )
-                }
-            }, { it -> showError(it) })
+                    }
+                },
+                { it -> showError(it) })
     }
 
     private fun showError(t: Throwable) {
         Toast.makeText(activity, "ERROR", Toast.LENGTH_SHORT).show()
     }
 
-    private fun showErrorKEKW(t: Throwable) {
-        Toast.makeText(activity, "ERROR (KEKW", Toast.LENGTH_SHORT).show()
+    private fun showErrorSecond(t: Throwable) {
+        Toast.makeText(activity, "ERROR (Second)", Toast.LENGTH_SHORT).show()
     }
 }
